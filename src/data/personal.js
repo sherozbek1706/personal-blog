c652aad08cc3cec8f2ce27d512f3d5ab4859f601
sherozbek1706 <baxtiyorovsherozbek1111@gmail.com>
2024-01-07 22:00:24 +0500

    Shaxsiy bazi ma'lumotlarni static qilib belgilab olaman.

---
+++ src/data/personal.js
@@ -0,0 +1,14 @@
+export const PersonalData = {
+  first_name: "She'rozbek",
+  last_name: "Baxtiyorov",
+  email: "baxtiyorovsherozbek1111@gmail.com",
+  birthday: "17-06-2005",
+  description: "FullStack Developer",
+  image: "https://avatars.githubusercontent.com/u/91471554?",
+  socials: [
+    {
+      icon: <></>,
+      url: "",
+    },
+  ],
+};
